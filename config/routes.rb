c5dfc96243f0bfc817303740609137ac9182a6f3
takumi.iwasaki <lcmwanol@gmail.com>
2020-03-23 21:37:52 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'mcroposts/create'
+  get 'mcroposts/destroy'
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
@@ -10,4 +12,5 @@ Rails.application.routes.draw do
   
   get "signup" , to: "users#new"
   resources :users, only: [:index, :show, :new, :create]
+  resources :microposts, only: [:create, :destroy]
 end
